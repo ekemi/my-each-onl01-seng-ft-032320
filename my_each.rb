0dbbd5f043a9eaec4dd382ce5af6076945e74a7e
edmundo papi <edmundopierre@yahoo.com>
2020-03-27 14:13:09 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+  len = array.length
+
+  counter = 0
+  while counter <= 0
+    yield array[counter]
+    counter+=1
+  end
+    
 end
